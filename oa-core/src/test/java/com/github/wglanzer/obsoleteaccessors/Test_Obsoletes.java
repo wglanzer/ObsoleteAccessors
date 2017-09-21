e21c6eb8650b322ca941c4e69fd54be6e33bf3b6
w.glanzer <w.glanzer@adito.de>
2017-09-21 16:22:24 +0200

    Missing import

--- oa-core/src/test/java/com/github/wglanzer/obsoleteaccessors/Test_Obsoletes.java
+++ oa-core/src/test/java/com/github/wglanzer/obsoleteaccessors/Test_Obsoletes.java
@@ -1,7 +1,6 @@
 package com.github.wglanzer.obsoleteaccessors;
 
 import com.github.wglanzer.obsoleteaccessors.api.*;
-import com.github.wglanzer.obsoleteaccessors.spi.IRegistryKey;
 import org.junit.*;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
